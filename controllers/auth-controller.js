b26a68de21f71dd9ebb3eb529079e158da38bb97
Weepy <imbrued.mitre@gmail.com>
2022-04-19 01:25:54 +0800

    added login with cookies

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -23,3 +23,44 @@ exports.registerUser = async (req, res, next) => {
     return next(error);
   }
 };
+
+// Authenticates a user, returns a cookie containing the access token if successful
+exports.login = async (req, res, next) => {
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email: email }).select('+password');
+    if (!user) {
+      return res.status(401).send({
+        message: 'Invalid email or password'
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send({
+        message: 'Invalid email or password'
+      });
+    }
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin
+      },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 // 1 day
+    });
+    res.status(200).send({
+      message: 'You have successfully logged in'
+    });
+
+  } catch (error) {
+    return next(error);
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const auth = require('../controllers/auth-controller');
 
 router.post('/register', auth.registerUser);
+router.post('/login', auth.login);
 
 module.exports = router;
