ec35649aabb543ddae23b47e93a5252fc5f724c3
Weepy <imbrued.mitre@gmail.com>
2022-04-19 16:16:30 +0800

    added getting user profile

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const user = require('../controllers/user-controller');
 
-
+router.get('/profile', user.viewProfile);
 
 module.exports = router;
